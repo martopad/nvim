1da6f73569a51970f88306177cf1c59de6b068f2
martopad <martin_luther_yung@outlook.com>
2024-11-23 20:27:01 +0100

    Inline suggestions improvement plugin

--- lua/plugins/completions.lua
+++ lua/plugins/completions.lua
@@ -49,5 +49,13 @@ return {
 				}),
 			})
 		end,
-	}
+	},
+    {
+        "rachartier/tiny-inline-diagnostic.nvim",
+        event = "VeryLazy", -- Or `LspAttach`
+        priority = 1000, -- needs to be loaded in first
+        config = function()
+            require('tiny-inline-diagnostic').setup()
+        end
+    },
 }

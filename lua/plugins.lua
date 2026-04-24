f748751fed70fd5fe9a338098373e32dc544faa2
martopad <martin_luther_yung@outlook.com>
2026-04-24 14:12:19 +0200

    Status line configuration
    Uses lualine plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -7,3 +7,10 @@ vim.pack.add({
     { src = "https://github.com/yorumicolors/yorumi.nvim", name = "yorumi" },
 })
 require("plugins/colorscheme")
+
+-- Status line
+vim.pack.add({
+    { src = "https://github.com/nvim-tree/nvim-web-devicons", name = "nvim-web-devicons" },
+    { src = "https://github.com/nvim-lualine/lualine.nvim", name = "lualine" },
+})
+require("plugins/status_line")

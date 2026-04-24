6f567fb001ce187b8e72739b9727c485f63fee12
martopad <martin_luther_yung@outlook.com>
2026-04-24 13:18:50 +0200

    include keymaps

--- init.lua
+++ init.lua
@@ -4,4 +4,5 @@
 vim.pack.add{{ src = "https://github.com/yorumicolors/yorumi.nvim", name = "yorumi" }}
 vim.cmd.colorscheme("yorumi")
 
+require("preferences_keymaps")
 require("preferences_options")

61c7e9bfca7a7b0c40477b47336f3b8f4a9f461d
martopad <martin_luther_yung@outlook.com>
2026-04-24 12:35:28 +0200

    Start from scratch
    It is better to nuke everything
    because there are many changes
    to nvim. I just did many of the
    things without understanding stuff.

--- lua/vim_options.lua
+++
@@ -1,9 +0,0 @@
-vim.cmd("set nowrap")
-vim.cmd("set number")
-vim.cmd("set expandtab")
-vim.cmd("set tabstop=4")
-vim.cmd("set softtabstop=4")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set smartindent")
-vim.cmd("set colorcolumn=88")
-vim.g.mapleader = " "

fcfbdac554ae72ecdb364b80578cb95a3dc039dd
martopad <martin_luther_yung@outlook.com>
2026-04-24 13:30:15 +0200

    Add auto commands
    1. Highlight visual feedback when yanking
    2. Restore cursore to last position when reopening files.
    3. Custom handling for non-code files

--- init.lua
+++ init.lua
@@ -4,5 +4,6 @@
 vim.pack.add{{ src = "https://github.com/yorumicolors/yorumi.nvim", name = "yorumi" }}
 vim.cmd.colorscheme("yorumi")
 
+require("preferences_autocmds")
 require("preferences_keymaps")
 require("preferences_options")

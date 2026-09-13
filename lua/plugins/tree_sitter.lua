5b26dbbc984f199ca75516ac4bb40cc42fc16a9c
Martin Luther Yung <martin_luther_yung@outlook.com>
2026-09-13 13:43:09 +0200

    LazyNvim migration: Tree-sitter

--- lua/plugins/tree_sitter.lua
+++ lua/plugins/tree_sitter.lua
@@ -1,12 +1,15 @@
 -- ============================================================================
--- Tree-sitter and Tree-sitter package management integrations
--- Plugin configuration: tree-sitter-manager
---
+-- Plugin Configuration: tree-sitter-manager
 -- ============================================================================
 
-require("tree-sitter-manager").setup({
-    ensure_installed = { "markdown", "markdown_inline", "vim", "yaml" },
-    border = "rounded",
-    auto_install = true,
-    highlight = true,
-})
+return {
+  {
+    "romus204/tree-sitter-manager.nvim",
+    opts = {
+      ensure_installed = { "markdown", "markdown_inline", "vim", "yaml" },
+      border = "rounded",
+      auto_install = true,
+      highlight = true,
+    },
+  }
+}

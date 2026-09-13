856abf6074a7d054e0faf782aa03de65f048478f
Martin Luther Yung <martin_luther_yung@outlook.com>
2026-09-13 13:43:09 +0200

    LazyNvim Migration: Colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,26 +2,36 @@
 -- Plugin Configuration: catppuccin
 -- ============================================================================
 
-require("catppuccin").setup({
-	flavour = "mocha",
-	color_overrides = {
-		all = {
-			-- make text slightly darker than white.
-			text = "#b8b8b8",
-		},
-		mocha = {
-			-- make background black for OLED.
-			base = "#000000",
-		},
-	},
-	integrations = {
-		-- Just use base catpuccin theme instead of applying catpuccin's nvimtree specific one.
-		nvimtree = false,
-		mini = {
-			enabled = true,
-			indentscope_color = "text",
-		},
-	},
-})
-
-vim.cmd.colorscheme("catppuccin")
+return {
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+      -- Configure the theme options here before loading it
+      require("catppuccin").setup({
+        flavour = "mocha",
+        color_overrides = {
+          all = {
+            -- make text slightly darker than white.
+            text = "#b8b8b8",
+          },
+          mocha = {
+            -- make background black for OLED.
+            base = "#000000",
+          },
+        },
+        integrations = {
+          -- Just use base catpuccin theme instead of applying catpuccin's nvimtree specific one.
+          nvimtree = false,
+          mini = {
+            enabled = true,
+            indentscope_color = "text",
+          },
+        },
+      })
+      -- Load the colorscheme
+      vim.cmd([[colorscheme catppuccin]])
+    end,
+  }
+}

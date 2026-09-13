f05cff0803e67825483e476e065ccb02d8b913aa
Martin Luther Yung <martin_luther_yung@outlook.com>
2026-09-13 13:43:09 +0200

    LazyNvim migration: Git Integrations

--- lua/plugins/git_integrations.lua
+++ lua/plugins/git_integrations.lua
@@ -1,9 +1,71 @@
 -- ============================================================================
--- Git integrations.
--- Plugin configuration: gitsigns
+-- Plugin Configuration: gitsigns
 -- ============================================================================
 
-require("gitsigns").setup({
-	signcolumn = true,
-	current_line_blame = false,
-})
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    keys = {
+      {
+        "]h",
+        function()
+          require("gitsigns").next_hunk()
+        end,
+        desc = "Next git hunk",
+      },
+      {
+        "[h",
+        function()
+          require("gitsigns").prev_hunk()
+        end,
+        desc = "Previous git hunk",
+      },
+      {
+        "<leader>hs",
+        function()
+          require("gitsigns").stage_hunk()
+        end,
+        desc = "Stage hunk",
+      },
+      {
+        "<leader>hr",
+        function()
+          require("gitsigns").reset_hunk()
+        end,
+        desc = "Reset hunk",
+      },
+      {
+        "<leader>hp",
+        function()
+          require("gitsigns").preview_hunk()
+        end,
+        desc = "Preview hunk",
+      },
+      {
+        "<leader>hb",
+        function()
+          require("gitsigns").blame_line({ full = true })
+        end,
+        desc = "Blame line",
+      },
+      {
+        "<leader>hB",
+        function()
+          require("gitsigns").toggle_current_line_blame()
+        end,
+        desc = "Toggle inline blame",
+      },
+      {
+        "<leader>hd",
+        function()
+          require("gitsigns").diffthis()
+        end,
+        desc = "Diff this",
+      },
+    },
+    opts = {
+      signcolumn = true,
+      current_line_blame = false,
+    },
+  }
+}
